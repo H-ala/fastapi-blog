f15d00a9e94f31b8f49c5ea3b3fd3064d7c96557
HoseinAlaee <hos.ala81@gmail.com>
2025-09-11 00:22:11 +0330

    initial commit

---
+++ blog/schemas.py
@@ -0,0 +1,52 @@
+from pydantic import BaseModel, Field
+from typing import List
+
+
+class BlogBase(BaseModel):
+    title: str = Field(default='blog' ,min_length=7 , max_length=120, description='the title of the blog')
+    body: str
+
+
+class Blog(BlogBase):
+   class Config():
+        orm_mode = True 
+
+
+class User(BaseModel):
+    name: str
+    email: str
+    password: str
+
+
+
+class ShowUser(BaseModel):
+    name: str
+    email: str
+    blogs: List[Blog] = []
+    class Config():
+        orm_mode = True
+
+
+ 
+class ShowBlog(BaseModel): 
+    title: str
+    body: str
+    creator: ShowUser
+    class Config():
+        orm_mode = True
+
+
+
+class Login(BaseModel):
+    username: str
+    password: str
+
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    email: str | None = None
